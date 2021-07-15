af91e04332f4a6d62035b70a823b3e0bd33d7978
109149 <109149qwe@gmail.com>
2021-07-15 18:12:23 +0400

    refactor: re-export

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,6 @@
 import { addSpaceWarp } from "./spaceWarp";
 import { addSocialButtons } from "./social";
 import { addGlowingText } from "./glowText";
+import * as utils from "./utils";
 
-export { addSpaceWarp, addSocialButtons, addGlowingText };
+export { addSpaceWarp, addSocialButtons, addGlowingText, utils };

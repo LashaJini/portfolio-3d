5bd6d51169c5d0e797db5029c7559a2e90614b48
109149 <109149qwe@gmail.com>
2021-07-16 00:06:47 +0400

    perf: reduce webpack bundle size
    Reduce bundle size by minification and removing source map in
    production. Extract css from js. Copy public directory's contents to
    build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,71 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
-const config = (env, argv) => {
-  const outDir = "build";
-  const PORT = 3000;
+const outDir = "build";
+const PORT = 3000;
 
-  return {
-    entry: "./index",
-    output: {
-      path: path.join(__dirname, outDir),
-      // filename: "[name].[chunkhash].bundle.js",
-      filename: "[name].bundle.js",
-    },
-    devServer: {
-      // contentBase: path.join(__dirname, outDir),
-      contentBase: path.join(__dirname, "/public/"),
-      port: PORT,
-      compress: true,
-    },
-    devtool: "source-map",
-    resolve: {
-      extensions: [".js"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"],
-        },
-        {
-          test: /\.pug$/,
-          use: ["pug-loader"],
-        },
-        {
-          test: /\.(jpe?g|png|gif|glb|gltf)$/,
-          use: ["file-loader"],
-        },
-        {
-          test: /\.(glsl)$/,
-          use: ["raw-loader"],
-        },
-      ],
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        template: "./views/pug/index.pug",
-        inject: true,
-      }),
+const config = {
+  entry: "./index",
+  output: {
+    path: path.join(__dirname, outDir),
+    // filename: "[name].[chunkhash].bundle.js",
+    filename: "[name].bundle.js",
+  },
+  devServer: {
+    // contentBase: path.join(__dirname, outDir),
+    contentBase: path.join(__dirname, "/public/"),
+    port: PORT,
+    compress: true,
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  optimization: {
+    minimize: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.pug$/,
+        use: ["pug-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(glb|glsl|glb)$/,
+        use: ["raw-loader"],
+      },
     ],
-  };
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "" }],
+      options: {
+        concurrency: 100,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./views/pug/index.pug",
+      inject: true,
+    }),
+  ],
 };
 
-module.exports = config;
+module.exports = (env, argv) => {
+  if (argv.mode === "development") {
+    config.devtool = "source-map";
+  }
+
+  return config;
+};

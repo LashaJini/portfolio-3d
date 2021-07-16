1871b592eb0e1e73a0594d4feec29837d25eb4c9
109149 <109149qwe@gmail.com>
2021-07-16 18:01:49 +0400

    fix: don't remove static folder

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 git checkout www && \
-  rm -rf static && \
   git merge master && \
   ls -1 static/js/ | awk '{print "static/js/"$1}' | xargs sed -i -r 's/(.*)("\/)[^portfolio\-3d](.*\/)/\1"\/portfolio-3d\/\3/' && \
   yarn build && \
